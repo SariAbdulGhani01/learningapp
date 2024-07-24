3fc8298e093d46f27d59b68745f8f31bad2dd466
SariAbdulGhani01 <sari.abdulghani@noema.net>
2024-07-24 16:32:30 +0300

    r

--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556c24b793571f6a2de950f654071eefda80b985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3846b6e3aff832b7b0291295309e12cbd8da0a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
